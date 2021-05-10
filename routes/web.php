64432f3bdeb3d5848cfcb29037d154367a30a192
G3t3xtra <nigeldorning@gmail.com>
2021-05-10 18:18:38 +0100

    refactor: redirected base route to the dashboard

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', '/dashboard');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', ExpenseTracker::class)->name('dashboard');
